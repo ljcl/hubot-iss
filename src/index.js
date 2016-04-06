f4ceb7d83e5d211025e73f68a7eb844059d0ba41
Luke Clark <contact@lukejclark.com>
2016-04-06 12:37:16 +1000

    improve iss location query

--- src/index.js
+++ src/index.js
@@ -4,7 +4,55 @@ import moment from 'moment-timezone'
 import request from 'request'
 import GeoCoder from 'node-geocoder'
 const geocoder = GeoCoder('google', 'http')
-// geocoder
+
+function astroViewer (options, cb) {
+  request({
+    url: 'http://astroviewer-sat2c.appspot.com/predictor',
+    qs: {
+      var: 'passesData',
+      lat: options.lat,
+      lon: options.lon,
+      name: options.name
+    },
+    headers: {
+      'User-Agent': 'request'
+    }
+  }, function (error, response, body) {
+    if (error) {
+      cb(error)
+    }
+    // Trim the javascript response
+    var raw = body.slice(17).slice(0, -2)
+    // make sure the keys have quotes around them
+    raw = raw.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":')
+    // turn the result into valid JSON (hopefully)
+    var info = JSON.parse(raw)
+    cb(false, info)
+  })
+}
+
+function listPasses (data, cb) {
+  var index = 0
+  var passes = data.passes
+  var newpasses = ''
+  if (passes.length === 0) {
+    newpasses += ':( No results found for **' + data.location.name + '**'
+  } else {
+    passes.map(function (obj) {
+      if (index === 0) {
+        newpasses += '**' + data.location.name + '** (' + obj.timezone + ')\n'
+      }
+      var dateFormat = 'YYYYMMDDHHmmss'
+      var newDateFormat = 'ddd MMM Do, hh:mma'
+      var begin = moment(obj.begin, dateFormat).format(newDateFormat)
+      var duration = moment.utc(moment(obj.end, dateFormat).diff(moment(obj.begin, dateFormat))).format('m\\m s\\s')
+      newpasses += begin + ' (' + duration + ')\n'
+      index++
+    })
+  }
+  newpasses += ':satellite:'
+  cb(false, newpasses)
+}
 
 module.exports = (robot) => {
   // Respond with the location and a map of the reported location of the ISS
@@ -28,70 +76,25 @@ module.exports = (robot) => {
     })
   })
 
-  request({
-    url: 'https://maps.googleapis.com/maps/api/timezone/json',
-    qs: { location: '39.6034810' + ',' + '-119.6822510', timestamp: '1331161200' }
-  }, function (err, resp, body) {
-    body = JSON.parse(body)
-  })
-
-  function timezoneId (location, cb) {
-    // Accepts location (lat,long) and unix timestamp and returns timezone info as a callback
-    var timestamp = Math.floor((new Date).getTime() / 1000)
-    request({
-      url: 'https://maps.googleapis.com/maps/api/timezone/json',
-      qs: {
-        location: location,
-        timestamp: timestamp // Doesn't matter, use whatever
-      }
-    }, function (err, resp, body) {
-      body = JSON.parse(body)
-      cb(body)
-    })
-  }
-
-  // https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200
-
-  robot.hear(/(when does the)? (iss) (flyover|fly over) (.*)/i, (res) => {
+  robot.hear(/(when does the)? (iss) (flyover|fly over|pass|pass over) (.*)/i, (res) => {
     var location = res.match[4]
-    var timeZoneId = 'Australia/Sydney'
     geocoder.geocode(location, function (err, resp) {
       var message = "Couldn't find a location by that name" // Default return message
       if (err) throw new Error(err)
       if (resp.length > 0) {
         // As long as there's at least one, we'll only ever use the first result though
         resp = resp[0]
-        request({
-          url: 'http://api.open-notify.org/iss-pass.json',
-          qs: { lat: resp.latitude, lon: resp.longitude, n: 35 }
-        }, function (error, response, body) {
+        astroViewer({lat: resp.latitude, lon: resp.longitude, name: resp.formattedAddress}, function (error, result) {
           if (error) throw new Error(error)
-          body = JSON.parse(body)
-          var riseTimes = body.response
-          var counter = 0
-          message = 'The next few passes for the ISS from *' + resp.formattedAddress + '* are:\n'
-          riseTimes.map(function (obj, index) {
-            // Make the returned data a little more readable
-            timezoneId(resp.latitude + ',' + resp.longitude, (timezone) => {
-              timeZoneId = timezone.timeZoneId
-              var risetime = moment.unix(obj.risetime)
-              var risehour = moment.tz(risetime, timeZoneId).hour()
-              var risetimeLocal = moment.tz(risetime, timeZoneId)
-              risetimeLocal = moment.unix(risetimeLocal).format('ddd MMM Do, hh:mma ZZ')
-              var duration = moment.duration(obj.duration, 'seconds').humanize()
-              if ((risehour >= 19)) {
-                message += risetimeLocal + ' (for ' + duration + ')\n'
-              }
-              counter++
-              if (counter >= riseTimes.length) {
-                message += ':satellite:'
-                res.send(message)
-              }
-            })
+          listPasses(result, function (err, message) {
+            if (err) throw new Error(err)
+            res.send(message)
+          // console.log(message)
           })
         })
       } else {
         res.send(message)
+      // console.log(message)
       }
     })
   })
